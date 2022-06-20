9615d36344f827b094819c73c4ea718ad36f6337
dandy.alv <dandy.alv@gmail.com>
2022-06-20 23:50:32 +0800

    final commit

--- script.js
+++ script.js
@@ -1,30 +1,134 @@
-let fetchData = async () => {
-    const url = "https://pokeapi.co/api/v2/pokemon/25"; 
+let fetchAll = async () => {
+    const url = "https://pokeapi.co/api/v2/pokemon?limit=30"; 
     const response = await fetch(url, {
         method: "GET"
     });
 
     const json = await response.json();
 
-    console.log(json);
+    allData = json.results;
 
-    return json;
+    return allData;
 }
 
 let displayResult = async () => {
-    const data = await fetchData();
-    let number = document.getElementById("number");
-    let name = document.getElementById("name");
-    let img = document.getElementById("img");
-    let type = document.getElementById("type");
-    let card = document.getElementById("card");
+    
+    const data = await fetchAll();
 
-    card.style.background = "#FFFF00";
-    card.style.display = "inline-block";
+    data.forEach( (res) => {
+        var urlPokemon = res.url;
+        console.log(urlPokemon);
 
-    number.innerHTML = `${data.id}: `;
-    name.innerHTML = data.name;
-    img.src = data.sprites.front_shiny;
-    type.innerHTML = `type: ${data.types[0].type.name}`;
+        let eachData = async () => {
+            const response = await fetch(urlPokemon, {
+                method: "GET"
+            });
+        
+            const dataPokemon = await response.json();
+        
+            console.log(dataPokemon);
 
+            if(dataPokemon.types[0].type.name == "grass"){
+                document.getElementById("cards").insertAdjacentHTML(
+                    'beforeend', 
+                    `<div class="card" style="background-color: #92BA92">
+                        <span class="number">${dataPokemon.id}: </span>
+                        <span class="name">${dataPokemon.name}</span>
+                        <div>
+                            <img class="img" src="${dataPokemon.sprites.front_shiny}" alt="">
+                        </div>
+                        <span class="type">type: ${dataPokemon.types[0].type.name}</span>
+                    </div>`
+                );
+            }else if(dataPokemon.types[0].type.name == "fire") {
+                document.getElementById("cards").insertAdjacentHTML(
+                    'beforeend', 
+                    `<div class="card" style="background-color: #FF7878">
+                        <span class="number">${dataPokemon.id}: </span>
+                        <span class="name">${dataPokemon.name}</span>
+                        <div>
+                            <img class="img" src="${dataPokemon.sprites.front_shiny}" alt="">
+                        </div>
+                        <span class="type">type: ${dataPokemon.types[0].type.name}</span>
+                    </div>`
+                );
+            }else if(dataPokemon.types[0].type.name == "water") {
+                document.getElementById("cards").insertAdjacentHTML(
+                    'beforeend', 
+                    `<div class="card" style="background-color: #8CC0DE">
+                        <span class="number">${dataPokemon.id}: </span>
+                        <span class="name">${dataPokemon.name}</span>
+                        <div>
+                            <img class="img" src="${dataPokemon.sprites.front_shiny}" alt="">
+                        </div>
+                        <span class="type">type: ${dataPokemon.types[0].type.name}</span>
+                    </div>`
+                );
+            }else if(dataPokemon.types[0].type.name == "bug") {
+                document.getElementById("cards").insertAdjacentHTML(
+                    'beforeend', 
+                    `<div class="card" style="background-color: #D1D1D1">
+                        <span class="number">${dataPokemon.id}: </span>
+                        <span class="name">${dataPokemon.name}</span>
+                        <div>
+                            <img class="img" src="${dataPokemon.sprites.front_shiny}" alt="">
+                        </div>
+                        <span class="type">type: ${dataPokemon.types[0].type.name}</span>
+                    </div>`
+                );
+            }else if(dataPokemon.types[0].type.name == "normal") {
+                document.getElementById("cards").insertAdjacentHTML(
+                    'beforeend', 
+                    `<div class="card" style="background-color: #F6E3C5">
+                        <span class="number">${dataPokemon.id}: </span>
+                        <span class="name">${dataPokemon.name}</span>
+                        <div>
+                            <img class="img" src="${dataPokemon.sprites.front_shiny}" alt="">
+                        </div>
+                        <span class="type">type: ${dataPokemon.types[0].type.name}</span>
+                    </div>`
+                );
+            }else if(dataPokemon.types[0].type.name == "poison") {
+                document.getElementById("cards").insertAdjacentHTML(
+                    'beforeend', 
+                    `<div class="card" style="background-color: #F9CEEE">
+                        <span class="number">${dataPokemon.id}: </span>
+                        <span class="name">${dataPokemon.name}</span>
+                        <div>
+                            <img class="img" src="${dataPokemon.sprites.front_shiny}" alt="">
+                        </div>
+                        <span class="type">type: ${dataPokemon.types[0].type.name}</span>
+                    </div>`
+                );
+            }else if(dataPokemon.types[0].type.name == "electric") {
+                document.getElementById("cards").insertAdjacentHTML(
+                    'beforeend', 
+                    `<div class="card" style="background-color: #F4E06D">
+                        <span class="number">${dataPokemon.id}: </span>
+                        <span class="name">${dataPokemon.name}</span>
+                        <div>
+                            <img class="img" src="${dataPokemon.sprites.front_shiny}" alt="">
+                        </div>
+                        <span class="type">type: ${dataPokemon.types[0].type.name}</span>
+                    </div>`
+                );
+            }else{
+                document.getElementById("cards").insertAdjacentHTML(
+                    'beforeend', 
+                    `<div class="card" style="background-color: #B09B71">
+                        <span class="number">${dataPokemon.id}: </span>
+                        <span class="name">${dataPokemon.name}</span>
+                        <div>
+                            <img class="img" src="${dataPokemon.sprites.front_shiny}" alt="">
+                        </div>
+                        <span class="type">type: ${dataPokemon.types[0].type.name}</span>
+                    </div>`
+                );
+            }
+
+        }
+
+        eachData();
+        
+    });
 }
